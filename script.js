52a462ad9a3ca57b597b0c2cbdb034061053fd95
Tobie Milford <tobie.milford@gmail.com>
2019-09-08 17:40:52 -0700

    HelpDesk: why only 1 YouTube video appending?

--- script.js
+++ script.js
@@ -1,7 +1,58 @@
 $('.form').submit(event => {
     event.preventDefault();
+    $('.content').empty();
     let query = $('.text_input').val();
-    console.log('blahblah');
+    formatQueries(query);
   });
 
+  // function sends "query" to three formatting
+  function formatQueries(input) {
+    youTubeFormatter(input);
+    // wikiFormatter(input);
+    // museScoreFormatter(input);
+  }
+
+  // function formats url for YouTube query
+  function youTubeFormatter(input) {
+    const apiKey = "AIzaSyAqrx3UI21YR8ip4LI7dtGwW_cXa_qrg44";
+    const params = {
+      q: `${input}`,
+      part: "snippet",
+      type: "video",
+      maxresults: 5,
+      key: apiKey,
+      order: "rating"
+    }
+    const youTubeBaseURL = "https://www.googleapis.com/youtube/v3/search?";
+    const queryString = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+    .join('&');
+    const finalYouTubeURL = youTubeBaseURL + queryString;
+    console.log(finalYouTubeURL);
+    YouTubeFetcher(finalYouTubeURL);
+  }
   
+  // function gets JSON object for YouTube query
+  function YouTubeFetcher(url) {
+    fetch(url)
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error (response.statusText);
+      })
+      .then (responseJson => YouTubeDisplayer(responseJson))
+      .catch(err => {
+        $('.content').text(`Something went wrong: ${err.message}`)})
+  }
+
+  // function updates page using YouTube JSON object
+  function YouTubeDisplayer(json) {
+    for (i = 0; i < json.items.length; i++)
+      var id = json.items[i].id.videoId;
+      $('.content').append(`<iframe width="400" height="300" src="https://www.youtube.com/embed/${id}"></iframe><br>`)
+      console.log(id);
+  }
+
+  // <iframe width="400" height="300" src="https://www.youtube.com/embed/${id}"></iframe>
+
+  // let videoId = ${json.items[i].id.videoId};
