d94d23d52de1852d8d29ff7b55a9ce47f8d170bb
Tobie Milford <tobie.milford@gmail.com>
2019-09-15 22:48:18 -0700

    9/15/19 10:47p + two input forms, edge case logic

--- script.js
+++ script.js
@@ -3,27 +3,27 @@
 $('.form').submit(event => {
     event.preventDefault();
     $('.content').empty();
-    let query = $('.text_input').val();
-    formatQueries(query);
+    let composerQuery = $('.composer_input').val();
+    let pieceQuery = $('.piece_input').val();
+    formatQueries(composerQuery, pieceQuery);
   });
 
-  // function sends "query" to three formatting
-  function formatQueries(input) {
-    wikiFormatter(input);
-    youTubeFormatter(input);
-    // museScoreFormatter(input);
+  // function sends both parts of query to two formatting functions
+  function formatQueries(composer, piece) {
+    wikiFormatter(composer, piece);
+    youTubeFormatter(composer, piece);
   }
 
   // function formats url for YouTube query
-  function youTubeFormatter(input) {
+  function youTubeFormatter(composer, piece) {
     const apiKey = "AIzaSyAqrx3UI21YR8ip4LI7dtGwW_cXa_qrg44";
     const params = {
-      q: `${input}`,
+      q: `${composer} ${piece}`,
       part: "snippet",
       type: "video",
       maxResults: 3,
       key: apiKey,
-      order: "rating"
+      order: "relevance"
     }
     const youTubeBaseURL = "https://www.googleapis.com/youtube/v3/search?";
     const queryString = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
@@ -49,32 +49,27 @@ $('.form').submit(event => {
 
   // function updates page using YouTube JSON object
   function YouTubeDisplayer(json) {
+    console.log(json);
     for (i = 0; i < json.items.length; i++) {
       var id = json.items[i].id.videoId;
       $('.content').append(`<iframe width="400" height="300" src="https://www.youtube.com/embed/${id}"></iframe><br>`)
     }
   }
 
-function wikiFormatter(input) {
-  // const params = {
-  //   titles: `${input}`,
-  //   format: "json",
-  //   action: "query",
-  //   prop: "extracts",
-  //   exlimit: "max",
-  //   origin: "*"
-    // explaintext, exintro, redirects=
-// }; 
-  
-  queryWikiUrl = input.replace(/ /g, "_");
-
-  // const baseWikiUrl = 'https://en.wikipedia.org/w/api.php?explaintext&exintro&redirects=1&'
-  const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
-  // const queryWikiUrl = Object.keys(params).map
-  //   (key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`).join('&');
-  const finalWikiUrl = baseWikiUrl + queryWikiUrl
-  wikiFetcher(finalWikiUrl);
-}
+function wikiFormatter(composer, piece) {
+  if (composer) {
+    let queryWikiUrl = `${piece} (${composer})`.replace(/ /g, "_");
+    const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+    const finalWikiUrl = baseWikiUrl + queryWikiUrl;
+    wikiFetcher(finalWikiUrl);
+  }
+  else {
+    let queryWikiUrl = `${piece}`.replace(/ /g, "_");
+    const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+    const finalWikiUrl = baseWikiUrl + queryWikiUrl;
+    wikiFetcher(finalWikiUrl);
+  }
+} 
 
 function wikiFetcher(wikiUrl) {
   console.log(wikiUrl);
@@ -87,7 +82,7 @@ function wikiFetcher(wikiUrl) {
     })
     .then (responseJson => wikiDisplayer(responseJson))
     .catch(err => {
-      $('.content').text(`No results. Try entering your request differently.`)
+      $('.content').text(`No results found in WikiPedia with your input.  Try entering your request differently.`)
     })
 }
 
@@ -96,21 +91,13 @@ function wikiDisplayer(json) {
   let wikiSummary = json.extract;
   let wikiTitle = json.titles.display;
   $('.content').append(`<h1>${wikiTitle}</h1>`)
-  $('.content').append(`<p>${wikiSummary}</p>`)  
+  $('.content').append(`<p>${wikiSummary}</p>`)
+  if (json.originalimage.source) {
+    $('.content').append(`<p><img src="${json.originalimage.source}"></p>`)
+  }
 }
 
 
 
 // To do: 
-  // 1) two inputs from user: Composer and name of piece?
-    // All words MUST be capitalized.
-    // Composer MUST be in parenthesis and MUST be after piece (unless specific name like "Well Tempered Clavier", which worked)
-
-  // Problem: above format doesn't work for all pieces.  ex String_Quartet_1_Beethoven
-      //Solution: if no results, either print nothing or print "Please try entering your query differently ex..."?
-  // Problem: some videos from YouTube won't play after being loaded
-
-  // Problem: some videos aren't in English
-  // Problem: 
-
-
+  // 3) use Promise.all {fetchers}.... .then (load stuff)
