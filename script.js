9af24dd317f5547c2d791584a35d2a5a0a239fb9
Tobie Milford <tobie.milford@gmail.com>
2019-09-16 20:58:47 -0700

    9/16/19 9p Sumbitting to TF - MVP stage

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 
 $('.form').submit(event => {
     event.preventDefault();
-    $('.content').empty();
+    $('.wikiContent').empty();
+    $('.youTubeContent').empty();
     let composerQuery = $('.composer_input').val();
     let pieceQuery = $('.piece_input').val();
     formatQueries(composerQuery, pieceQuery);
@@ -13,6 +14,48 @@ $('.form').submit(event => {
     wikiFormatter(composer, piece);
     youTubeFormatter(composer, piece);
   }
+
+function wikiFormatter(composer, piece) {
+  if (composer) {
+    let queryWikiUrl = `${piece} (${composer})`.replace(/ /g, "_");
+    const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+    const finalWikiUrl = baseWikiUrl + queryWikiUrl;
+    console.log(finalWikiUrl);
+    wikiFetcher(finalWikiUrl);
+  }
+  else {
+    let queryWikiUrl = `${piece}`.replace(/ /g, "_");
+    const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
+    const finalWikiUrl = baseWikiUrl + queryWikiUrl;
+    wikiFetcher(finalWikiUrl);
+  }
+} 
+
+function wikiFetcher(wikiUrl) {
+  fetch(wikiUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error (response.statusText);
+    })
+    .then (responseJson => wikiDisplayer(responseJson))
+    .catch(err => {
+      $('.wikiContent').text(`No results found in WikiPedia with your input.  Try entering your request differently.`)
+    })
+}
+
+function wikiDisplayer(json) {
+  console.log(json);
+  let wikiSummary = json.extract;
+  let wikiTitle = json.titles.display;
+  $('.wikiContent').append(`<h1>${wikiTitle}</h1>`)
+  $('.wikiContent').append(`<p>${wikiSummary}</p>`)
+  if (json.originalimage) {
+    $('.wikiContent').append(`<p><img src="${json.originalimage.source}"></p>`)
+  }
+}
+
 
   // function formats url for YouTube query
   function youTubeFormatter(composer, piece) {
@@ -44,7 +87,7 @@ $('.form').submit(event => {
       })
       .then (responseJson => YouTubeDisplayer(responseJson))
       .catch(err => {
-        $('.content').text(`Something went wrong: ${err.message}`)})
+        $('.youTubeContent').text(`Something went wrong with YouTube search.}`)})
   }
 
   // function updates page using YouTube JSON object
@@ -52,53 +95,15 @@ $('.form').submit(event => {
     console.log(json);
     for (i = 0; i < json.items.length; i++) {
       var id = json.items[i].id.videoId;
-      $('.content').append(`<iframe width="400" height="300" src="https://www.youtube.com/embed/${id}"></iframe><br>`)
+      console.log(id);
+      $('.youTubeContent').append(`<iframe width="400" height="300" src="https://www.youtube.com/embed/${id}"></iframe><br>`)
     }
   }
-
-function wikiFormatter(composer, piece) {
-  if (composer) {
-    let queryWikiUrl = `${piece} (${composer})`.replace(/ /g, "_");
-    const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
-    const finalWikiUrl = baseWikiUrl + queryWikiUrl;
-    wikiFetcher(finalWikiUrl);
-    console.log(wikiFetcher);
-  }
-  else {
-    let queryWikiUrl = `${piece}`.replace(/ /g, "_");
-    const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
-    const finalWikiUrl = baseWikiUrl + queryWikiUrl;
-    wikiFetcher(finalWikiUrl);
-  }
-} 
-
-function wikiFetcher(wikiUrl) {
-  console.log(wikiUrl);
-  fetch(wikiUrl)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      throw new Error (response.statusText);
-    })
-    .then (responseJson => wikiDisplayer(responseJson))
-    .catch(err => {
-      $('.content').text(`No results found in WikiPedia with your input.  Try entering your request differently.`)
-    })
-}
-
-function wikiDisplayer(json) {
-  console.log(json);
-  let wikiSummary = json.extract;
-  let wikiTitle = json.titles.display;
-  $('.content').append(`<h1>${wikiTitle}</h1>`)
-  $('.content').append(`<p>${wikiSummary}</p>`)
-  if (json.originalimage.source) {
-    $('.content').append(`<p><img src="${json.originalimage.source}"></p>`)
-  }
-}
 
 
 
 // To do: 
-  // 3) use Promise.all {fetchers}.... .then (load stuff)
+  // 0) ensure user input is capitalized
+  // 1) use Promise.all {fetchers}.... .then (load stuff) to prevent diff loading experiences?
+  // 2) Create logic for "if no match (i.e. Bach + Well-Tempered Clavier), try without composer"
+
