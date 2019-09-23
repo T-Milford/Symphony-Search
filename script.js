fecb26f5e08fda47e33e20d673680011daf7b3b5
Tobie Milford <tobie.milford@gmail.com>
2019-09-22 21:44:33 -0700

    9/22 Want to: animage scroll, horiz divs @ large

--- script.js
+++ script.js
@@ -21,49 +21,9 @@ function wikiFormatter(composer, piece) {
     const finalWikiUrl = baseWikiUrl + queryWikiUrl;
     console.log(finalWikiUrl);
     wikiFetcher(finalWikiUrl);
-  
-  // else {
-  //   let queryWikiUrl = `${piece}`.replace(/ /g, "_");
-  //   const baseWikiUrl = 'https://en.wikipedia.org/api/rest_v1/page/summary/'
-  //   const finalWikiUrl = baseWikiUrl + queryWikiUrl;
-  //   wikiFetcher(finalWikiUrl, piece);
-  // }
 }
 
-function wikiFetcher(wikiUrl) {
-  fetch(wikiUrl)
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      }
-      //not sure how below line works
-      throw new Error (response.statusText);
-    })
-    .then (responseJson => wikiDisplayer(responseJson))
-    .catch(err => {
-      $('.wikiTitle').append(
-      `<div class="instructions"><h3>No results found.</h3>  
-      <h3>Please:</h3>
-      <ul class="instructions_list">
-        <li>Verify that your composer wrote a symphony of that number.</li>
-        <li>Verify that you have spelled his/her name correctly.</li>
-      </ul>
-    </div>.`)
-    })
-}
-
-function wikiDisplayer(json) {
-  console.log(json);
-  let wikiSummary = json.extract;
-  let wikiTitle = json.titles.display;
-  $('.wikiTitle').append(`<h2>${wikiTitle}</h2>`)
-  if (json.originalimage) {
-    $('.wikiImage').append(`<p><img class="appendedImage" src="${json.originalimage.source}"></p>`)
-  }
-  $('.wikiSummary').append(`<p class="appendedSummary">${wikiSummary}</p>`)  
-}
-
-  // function formats url for YouTube query
+// function formats url for YouTube query
   function youTubeFormatter(composer, piece) {
     const apiKey = "AIzaSyAqrx3UI21YR8ip4LI7dtGwW_cXa_qrg44";
     const params = {
@@ -81,34 +41,61 @@ function wikiDisplayer(json) {
     console.log(finalYouTubeURL);
     YouTubeFetcher(finalYouTubeURL);
   }
-  
-  // function gets JSON object for YouTube query
-  function YouTubeFetcher(url) {
-    fetch(url)
-      .then(response => {
-        if (response.ok) {
-          return response.json();
-        }
-        throw new Error (response.statusText);
-      })
-      .then (responseJson => YouTubeDisplayer(responseJson))
-      .catch(err => {
-        $('.youTubeContent').text(`Something went wrong with YouTube search.`)})
-  }
 
-  // function updates page using YouTube JSON object
-  function YouTubeDisplayer(json) {
+
+function wikiFetcher(wikiUrl) {
+  fetch(wikiUrl)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      //not sure how below line works
+      throw new Error (response.statusText);
+    })
+    .then (responseJson => wikiDisplayer(responseJson))
+    .catch(err => {
+      $('.wikiTitle').append(
+      `<div class="instructions"><h3>No results found in Wikipedia.</h3>  
+      <h3>Please:</h3>
+      <ul class="instructions_list">
+        <li>Verify that you have spelled last name correctly.</li>
+        <li>Verify that your composer wrote a symphony of that number.</li>
+        <li>See YouTube results below for ideas.</li>
+      </ul>
+    </div>.`)
+    })
+}
+
+// function gets JSON object for YouTube query
+function YouTubeFetcher(url) {
+  fetch(url)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      throw new Error (response.statusText);
+    })
+    .then (responseJson => YouTubeDisplayer(responseJson))
+    .catch(err => {
+      $('.youTubeContent').text(`Quota limit exceeded for YouTube search.  Try again later.`)})
+}
+
+function wikiDisplayer(json) {
+  console.log(json);
+  let wikiSummary = json.extract;
+  let wikiTitle = json.titles.display;
+  $('.wikiTitle').append(`<h1>${wikiTitle}</h1>`)
+  if (json.originalimage) {
+    $('.wikiImage').append(`<p><img class="appendedImage" src="${json.originalimage.source}" alt="Image of composer, score, etc."></p>`)
+  }
+  $('.wikiSummary').append(`<p class="appendedSummary">${wikiSummary}</p>`)  
+}
+
+// function updates page using YouTube JSON object
+function YouTubeDisplayer(json) {
     console.log(json);
     for (i = 0; i < json.items.length; i++) {
       var id = json.items[i].id.videoId;
-      $('.youTubeContent').append(`<iframe class="appendedVideo" width="400" height="300" src="https://www.youtube.com/embed/${id}"></iframe><br>`)
+      $('.youTubeContent').append(`<iframe class="appendedVideo" width="250" height="200" src="https://www.youtube.com/embed/${id}"></iframe><br>`)
     }
   }
-
-// To do: 
-
-  // 1) Create logic for "if no match (i.e. Bach + Well-Tempered Clavier), try without composer"
-  // 2) Make placeholder text disappear when either field is clicked.
-  
-
-
